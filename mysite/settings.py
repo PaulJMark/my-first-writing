f9cfe1a781ba61feca650d90e18bee9bfb513d89
Paul Mark <pauljmark@gmail.com>
2024-11-08 15:14:43 +0000

    Updated secret key

--- mysite/settings.py
+++ mysite/settings.py
@@ -20,10 +20,16 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-eod=k0ks7%ac9l)18-i%x*yuz2i^hw-*=m5kgstshimi31!(+u"
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+SECRET_KEY = os.environ['SECRET_KEY']
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get('DEBUG') == 'True'
 
 ALLOWED_HOSTS = ['localhost', '127.0.0.1', '.pythonanywhere.com']
 
