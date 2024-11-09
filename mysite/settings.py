222bac8f49e426b0d296bc1012c2b213850731cf
Paul Mark <pauljmark@gmail.com>
2024-11-09 14:00:29 +0000

    Adjusting recommended settings for secret_key on pythonanythwere.com

--- mysite/settings.py
+++ mysite/settings.py
@@ -21,12 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 import os
-from dotenv import load_dotenv
-
-load_dotenv()
-
-SECRET_KEY = os.environ['SECRET_KEY']
-
+SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG') == 'True'
